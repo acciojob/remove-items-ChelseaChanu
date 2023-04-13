0cac43321234e9848f4717a97a10a0b54bceacd5
Chelsea <88430660+ChelseaChanu@users.noreply.github.com>
2023-04-13 12:43:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const button = document.getElementsByTagName("input")[0];
+const select = document.getElementById("colorSelect");
+
+button.addEventListener("click",()=>{
+    select.options[select.selectedIndex].remove();
+});
